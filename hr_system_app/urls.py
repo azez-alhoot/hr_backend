f2d23c90bd2c33854d3a5f881320a0e413bdc2a5
azez-alhoot <azezalhoot82@yahoo.com>
2022-06-02 16:30:28 +0300

    created resume download view

--- hr_system_app/urls.py
+++ hr_system_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateCandidate, CandidatesList
+from .views import CreateCandidate, CandidatesList, DownloadCandidateResume
 
 urlpatterns = [
     path('create/candidate/', CreateCandidate.as_view(), name='create-candidate'),
     path('candidates/list', CandidatesList.as_view(), name='candidate-list'),
+    path('download/resume/<int:id>/', DownloadCandidateResume.as_view(), name='download-resume')
 ]
--- hr_system_app/views.py
+++ hr_system_app/views.py
@@ -1,3 +1,5 @@
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -16,3 +18,15 @@ class CandidatesList(ListAPIView):
     permission_classes = [permissions.IsAdminUser]
     queryset = Candidate.objects.all().order_by('-registration_date')
     serializer_class = CandidatesListSerializer
+
+
+class DownloadCandidateResume(ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, id, format=None):
+        queryset = Candidate.objects.get(id=id)
+        resume = queryset.resume.path
+        document = open(resume, 'rb')
+        response = HttpResponse(FileWrapper(document), content_type='application/msword')
+        response['Content-Disposition'] = 'filename="%s"' % queryset.resume.name
+        return response
